e9383fbbe9116c4eabe475b471597ec85dacf767
ipivtorak <yuriy.pivtorak@gmail.com>
2023-11-28 17:43:06 +0100

    cleanup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
-import React from 'react'
-import './App.scss'
-
-import MessageWindow from './components/MessageWindow'
-import TextBar from './components/TextBar'
+import React from 'react';
+import MessageWindow from './components/MessageWindow';
+import TextBar from './components/TextBar';
+import './App.scss';
 
 const App = () => (
     <div className='container'>
--- src/components/TextBarInput/index.js
+++ src/components/TextBarInput/index.js
@@ -1,18 +1,16 @@
 import React from 'react';
 import './styles.scss';
 
-const TextBarInput = ({ value, onChange, placeholder, onKeyDown, isError }) => {
-    return (
-        <input
-            value={value}
-            onChange={onChange}
-            className={`textbar-input ${isError ? 'input-error' : ''}`}
-            type='text'
-            onKeyDown={onKeyDown}
-            maxLength={255}
-            placeholder={placeholder}
-        />
-    )
-};
+const TextBarInput = ({ value, onChange, placeholder, onKeyDown, isError }) => (
+    <input
+        value={value}
+        onChange={onChange}
+        className={`textbar-input ${isError ? 'input-error' : ''}`}
+        type='text'
+        onKeyDown={onKeyDown}
+        maxLength={placeholder === 'text' && 255}
+        placeholder={placeholder}
+    />
+);
 
 export default TextBarInput;
